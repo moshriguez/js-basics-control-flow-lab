8dbb263d3718b0af6d60d33f90d064d7917210fd
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-04-05 12:56:24 -0700

    commit tree 166df23cb8a6f2383f1c64881ad82346df36f470

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+  let message;
+  if (num <= 400) {
+    message = 'This one is on me!';
+  } else if (num > 2000 && num <= 2500) {
+    message = 'I will gladly take your thirty bucks.';
+  } else if (num > 2500) {
+    message = 'No can do.';
+  };
+  return message;
+};
+
+function ternaryCheckCity(city){
+  return city === 'NYC'? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
